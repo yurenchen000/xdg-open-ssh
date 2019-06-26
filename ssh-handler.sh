65d20b431c5c8b763dea8996bfe67caff2d988af
yurenchen <yurenchen@yeah.net>
2019-06-26 15:38:00 +0800

    U ssh-handler.sh: add parseURL support ssh://user:pass@host:port url scheme

--- ssh-handler.sh
+++ ssh-handler.sh
@@ -5,8 +5,54 @@
 
 #d=${1#ssh://}
 #gnome-terminal -e "ssh $d" &
-terminator -e "ssh $d" &
+#terminator -e "ssh $d" &
 
 
+# xdg-open ssh://user:pass@host:port
+parseURL(){
+	local s=${1##ssh://}
+	local a=${s%%@*}
+	[ "$a" == "$s" ] && a=''
+	local h=${s##*@}
+
+	local user=${a%%:*}
+	local pass=${a##*:}
+	[ "$pass" == "$a" ] && pass=''
+	local host=${h%%:*}
+	local port=${h##*:}
+	[ "$port" == "$h" ] && port=''
+
+	decodeURIComponent(){
+	  echo "$1" | sed -e 's/%\([0-9A-F][0-9A-F]\)/\\\x\1/g'  | xargs -0 printf "%b";
+	}
+
+	pass=`decodeURIComponent "$pass"`
+
+	local cmd="$host"
+	[ -n "$user" ] && cmd="$user@$cmd"
+	[ -n "$port" ] && cmd="$cmd -p $port"
+	cmd="ssh $cmd"
+	cmd="$cmd -o StrictHostKeyChecking=no"
+	[ -n "$pass" ] && cmd="sshpass -p '$pass' $cmd"
+
+	echo "url: $1"
+	echo "cmd: $cmd"
+	echo
+	#$cmd
+
+	#terminator -e "$cmd" &
+	gnome-terminal -e "$cmd"
+	#echo "ret: $?"
+}
+
+## test
+# parseURL 'ssh://host'
+# parseURL 'ssh://host:port'
+# parseURL 'ssh://aaa@host:port'
+# parseURL 'ssh://aaa:123@host:port'
+# parseURL 'ssh://aaa:%25aa%25bb!%40%23%24%25%5E%26*()%3F%20%20%5C%2F@host:port'
+# parseURL 'ssh://chen:abc@10.1.1.30:1234'
+
+parseURL "$1"
 
 
