052262112ac0eb51ffd1fb4068c9aed485ae4289
yurenchen <yurenchen@yeah.net>
2019-09-14 20:14:16 +0800

    U install.bat: add cmdline arg --install

--- win/install.bat
+++ win/install.bat
@@ -4,8 +4,15 @@ echo cd  : "%cd%"
 echo dir : "%~dp0"
 echo file: "%~f0"
 
-REM reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%cd%\ssh_url_parser.bat\" %%1" -f
-reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%~dp0ssh_url_parser.bat\" %%1" -f
-REM reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%~f0\" %%1" -f
+echo arg1: %1
+echo.
+
+if "%1" == "--install" (
+	echo "install reg info.."
+	REM reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%cd%\ssh_url_parser.bat\" %%1" -f
+	reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%~dp0ssh_url_parser.bat\" %%1" -f
+	REM reg add HKCU\Software\Classes\ssh\shell\open\command /ve  /t REG_SZ  /d "\"%~f0\" %%1" -f
+	exit /b
+)
 
 reg query HKCU\Software\Classes\ssh\shell\open\command /ve
